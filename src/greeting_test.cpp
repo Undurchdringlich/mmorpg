73ada8c9b6533e26c50f06ea644ae852c8d69254
Undurchdringlich <Bookmester@yahoo.com>
2026-02-08 15:06:12 +0300

    Create userver project

---
+++ src/greeting_test.cpp
@@ -0,0 +1,13 @@
+#include <greeting.hpp>
+
+#include <userver/utest/utest.hpp>
+
+using mmorpg::UserType;
+
+UTEST(SayHelloTo, Basic) {
+    EXPECT_EQ(mmorpg::SayHelloTo("Developer", UserType::kFirstTime), "Hello, Developer!\n");
+    EXPECT_EQ(mmorpg::SayHelloTo({}, UserType::kFirstTime), "Hello, unknown user!\n");
+
+    EXPECT_EQ(mmorpg::SayHelloTo("Developer", UserType::kKnown), "Hi again, Developer!\n");
+    EXPECT_EQ(mmorpg::SayHelloTo({}, UserType::kKnown), "Hi again, unknown user!\n");
+}
